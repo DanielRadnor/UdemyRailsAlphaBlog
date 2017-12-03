05aa7d729d8d19838ab9d7f9c029599da2238967
Daniel Radnor <dannyradnor@hotmail.com>
2017-12-03 09:18:34 +0000

    Add partials and destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,47 @@
 class ArticlesController < ApplicationController
   
+  def index
+    @articles = Article.all  
+  end
+  
   def new
-    @article = Article.new
+    #this creates the new row in the articles database so that the form can be created
+    @article = Article.new 
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
   end
   
   def create
     #render plain: params[:article].inspect
+    #this populates the newly created row in the articles database with the information from the form
     @article = Article.new(article_params)
-    @article.save
-    redicet_to articles_show(@article)
+    if @article.save
+      #flash[:notice] = "Article was saved successfully"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end    
+  end
+  
+    def update
+      @article = Article.find(params[:id])
+      if @article.update(article_params)
+        redirect_to article_path(@article)
+      else
+         render 'edit'
+      end
+    end
+  
+  def show
+    @article = Article.find(params[:id])    
+  end
+  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
   
   private
